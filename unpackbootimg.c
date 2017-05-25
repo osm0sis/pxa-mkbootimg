623b2be942100a5e15e7b48ec4a5fa26897f1771
osm0sis <osm0sis@outlook.com>
2017-05-25 01:57:13 -0300

    unpackbootimg: update supported output, fix support for Tab 4 (degas) - update Android magic seeklimit to a more current 4096 for a bit of future proofing - go back to using "-zImage" filename suffix for standard naming (Tab 4 is zImage, Xcover 3 is uImage'd Image.gz) - rename output files, adjust screen output and cosmetic changes to match my more updated AOSP fork - add "-board" name, and "-kerneloff" offset dumping - test remaining data length to first check if a signature likely exists, then test for SEANDROIDENFORCE string to differentiate between the Xcover 3 (272 bytes due to this string) and Tab 4 (256 bytes); this adds support for images for this board that have no signature appended, and support for both known signature lengths

--- unpackbootimg.c
+++ unpackbootimg.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <limits.h>
 #include <libgen.h>
+#include <sys/stat.h>
 
 #include "mincrypt/sha.h"
 #include "bootimg.h"
@@ -52,6 +53,7 @@ int main(int argc, char** argv)
     char* directory = "./";
     char* filename = NULL;
     int pagesize = 0;
+    int base = 0;
 
     argc--;
     argv++;
@@ -81,29 +83,36 @@ int main(int argc, char** argv)
 
     //printf("Reading header...\n");
     int i;
-    for (i = 0; i <= 512; i++) {
+    int seeklimit = 4096;
+    for (i = 0; i <= seeklimit; i++) {
         fseek(f, i, SEEK_SET);
         fread(tmp, BOOT_MAGIC_SIZE, 1, f);
         if (memcmp(tmp, BOOT_MAGIC, BOOT_MAGIC_SIZE) == 0)
             break;
     }
     total_read = i;
-    if (i > 512) {
+    if (i > seeklimit) {
         printf("Android boot magic not found.\n");
         return 1;
     }
     fseek(f, i, SEEK_SET);
-    printf("Android magic found at: %d\n", i);
+    if (i > 0) {
+        printf("Android magic found at: %d\n", i);
+    }
 
     fread(&header, sizeof(header), 1, f);
+    base = header.kernel_addr - 0x00008000;
     printf("BOARD_KERNEL_CMDLINE %s\n", header.cmdline);
-    printf("BOARD_KERNEL_BASE %08x\n", header.kernel_addr - 0x00008000);
-    printf("BOARD_RAMDISK_OFFSET %08x\n", header.ramdisk_addr - header.kernel_addr + 0x00008000);
-    printf("BOARD_SECOND_OFFSET %08x\n", header.second_addr - header.kernel_addr + 0x00008000);
-    printf("BOARD_TAGS_OFFSET %08x\n",header.tags_addr - header.kernel_addr + 0x00008000);
+    printf("BOARD_KERNEL_BASE %08x\n", base);
+    printf("BOARD_NAME %s\n", header.name);
     printf("BOARD_PAGE_SIZE %d\n", header.page_size);
-    printf("BOARD_SECOND_SIZE %d\n", header.second_size);
-    printf("BOARD_DT_SIZE %d\n", header.dt_size);
+    printf("BOARD_KERNEL_OFFSET %08x\n", header.kernel_addr - base);
+    printf("BOARD_RAMDISK_OFFSET %08x\n", header.ramdisk_addr - base);
+    printf("BOARD_SECOND_OFFSET %08x\n", header.second_addr - base);
+    printf("BOARD_TAGS_OFFSET %08x\n",header.tags_addr - base);
+    if (header.dt_size != 0) {
+        printf("BOARD_DT_SIZE %d\n", header.dt_size);
+    }
     printf("BOARD_UNKNOWN %08x\n", header.unknown);
     
     if (pagesize == 0) {
@@ -115,40 +124,17 @@ int main(int argc, char** argv)
     strcat(tmp, "-cmdline");
     write_string_to_file(tmp, header.cmdline);
     
+    //printf("board...\n");
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-board");
+    write_string_to_file(tmp, header.name);
+    
     //printf("base...\n");
     sprintf(tmp, "%s/%s", directory, basename(filename));
     strcat(tmp, "-base");
     char basetmp[200];
-    sprintf(basetmp, "%08x", header.kernel_addr - 0x00008000);
+    sprintf(basetmp, "%08x", base);
     write_string_to_file(tmp, basetmp);
-
-    //printf("ramdisk_offset...\n");
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-ramdisk_offset");
-    char ramdisktmp[200];
-    sprintf(ramdisktmp, "%08x", header.ramdisk_addr - header.kernel_addr + 0x00008000);
-    write_string_to_file(tmp, ramdisktmp);
-
-    //printf("second_offset...\n");
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-second_offset");
-    char secondtmp[200];
-    sprintf(secondtmp, "%08x", header.second_addr - header.kernel_addr + 0x00008000);
-    write_string_to_file(tmp, secondtmp);
-
-    //printf("tags_offset...\n");
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-tags_offset");
-    char tagstmp[200];
-    sprintf(tagstmp, "%08x", header.tags_addr - header.kernel_addr + 0x00008000);
-    write_string_to_file(tmp, tagstmp);
-
-    //printf("store unknown value...\n");
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-unknown");
-    char unknownstmp[200];
-    sprintf(unknownstmp, "%08x", header.unknown);
-    write_string_to_file(tmp, unknownstmp);
 
     //printf("pagesize...\n");
     sprintf(tmp, "%s/%s", directory, basename(filename));
@@ -156,13 +142,48 @@ int main(int argc, char** argv)
     char pagesizetmp[200];
     sprintf(pagesizetmp, "%d", header.page_size);
     write_string_to_file(tmp, pagesizetmp);
-    
+
+    //printf("kerneloff...\n");
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-kerneloff");
+    char kernelofftmp[200];
+    sprintf(kernelofftmp, "%08x", header.kernel_addr - base);
+    write_string_to_file(tmp, kernelofftmp);
+
+    //printf("ramdiskoff...\n");
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-ramdiskoff");
+    char ramdisktmp[200];
+    sprintf(ramdisktmp, "%08x", header.ramdisk_addr - base);
+    write_string_to_file(tmp, ramdisktmp);
+
+    //printf("secondoff...\n");
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-secondoff");
+    char secondtmp[200];
+    sprintf(secondtmp, "%08x", header.second_addr - base);
+    write_string_to_file(tmp, secondtmp);
+
+    //printf("tagsoff...\n");
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-tagsoff");
+    char tagstmp[200];
+    sprintf(tagstmp, "%08x", header.tags_addr - base);
+    write_string_to_file(tmp, tagstmp);
+
+    //printf("unknown...\n");
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-unknown");
+    char unknownstmp[200];
+    sprintf(unknownstmp, "%08x", header.unknown);
+    write_string_to_file(tmp, unknownstmp);
+
     total_read += sizeof(header);
     //printf("total read: %d\n", total_read);
     total_read += read_padding(f, sizeof(header), pagesize);
 
     sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-uImage");
+    strcat(tmp, "-zImage");
     FILE *k = fopen(tmp, "wb");
     byte* kernel = (byte*)malloc(header.kernel_size);
     //printf("Reading kernel...\n");
@@ -174,58 +195,73 @@ int main(int argc, char** argv)
     //printf("total read: %d\n", header.kernel_size);
     total_read += read_padding(f, header.kernel_size, pagesize);
 
-
+    sprintf(tmp, "%s/%s", directory, basename(filename));
+    strcat(tmp, "-ramdisk.gz");
+    FILE *r = fopen(tmp, "wb");
     byte* ramdisk = (byte*)malloc(header.ramdisk_size);
     //printf("Reading ramdisk...\n");
     fread(ramdisk, header.ramdisk_size, 1, f);
     total_read += header.ramdisk_size;
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    if(ramdisk[0] == 0x02 && ramdisk[1]== 0x21)
-        strcat(tmp, "-ramdisk.lz4");
-    else
-        strcat(tmp, "-ramdisk.gz");
-    FILE *r = fopen(tmp, "wb");
     fwrite(ramdisk, header.ramdisk_size, 1, r);
     fclose(r);
 
+    //printf("total read: %d\n", header.ramdisk_size);
     total_read += read_padding(f, header.ramdisk_size, pagesize);
 
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-second");
-    FILE *s = fopen(tmp, "wb");
-    byte* second = (byte*)malloc(header.second_size);
-    //printf("Reading second...\n");
-    fread(second, header.second_size, 1, f);
-    total_read += header.second_size;
-    fwrite(second, header.second_size, 1, r);
-    fclose(s);
+    if (header.second_size != 0) {
+        sprintf(tmp, "%s/%s", directory, basename(filename));
+        strcat(tmp, "-second");
+        FILE *s = fopen(tmp, "wb");
+        byte* second = (byte*)malloc(header.second_size);
+        //printf("Reading second...\n");
+        fread(second, header.second_size, 1, f);
+        total_read += header.second_size;
+        fwrite(second, header.second_size, 1, r);
+        fclose(s);
+    }
 
+    //printf("total read: %d\n", header.second_size);
     total_read += read_padding(f, header.second_size, pagesize);
 
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-dt");
-    FILE *d = fopen(tmp, "wb");
-    byte* dt = (byte*)malloc(header.dt_size);
-    //printf("Reading dt...\n");
-    fread(dt, header.dt_size, 1, f);
-    total_read += header.dt_size;
-    fwrite(dt, header.dt_size, 1, r);
-    fclose(d);
+    if (header.dt_size != 0) {
+        sprintf(tmp, "%s/%s", directory, basename(filename));
+        strcat(tmp, "-dt");
+        FILE *d = fopen(tmp, "wb");
+        byte* dt = (byte*)malloc(header.dt_size);
+        //printf("Reading dt...\n");
+        fread(dt, header.dt_size, 1, f);
+        total_read += header.dt_size;
+        fwrite(dt, header.dt_size, 1, r);
+        fclose(d);
+    }
 
+    //printf("total read: %d\n", header.dt_size);
     total_read += read_padding(f, header.dt_size, pagesize);
 
-    sprintf(tmp, "%s/%s", directory, basename(filename));
-    strcat(tmp, "-signature");
-    FILE *fsig = fopen(tmp, "wb");
-    byte* bsig = (byte*)malloc(272);
-    //printf("Reading signature...\n");
-    fread(bsig, 272, 1, f);
-    total_read += 272;
-    fwrite(bsig, 272, 1, r);
-    fclose(fsig);
-    
+    struct stat st;
+    stat(filename, &st);
+    if (st.st_size >= total_read + 256) {
+        int sig_size = 0;
+        fread(tmp, 16, 1, f);
+        if (memcmp(tmp, "SEANDROIDENFORCE", 16) == 0) {
+            sig_size = 272;
+        } else {
+            sig_size = 256;
+        }
+        fseek(f, total_read, SEEK_SET);
+        sprintf(tmp, "%s/%s", directory, basename(filename));
+        strcat(tmp, "-signature");
+        FILE *fsig = fopen(tmp, "wb");
+        byte* bsig = (byte*)malloc(sig_size);
+        //printf("Reading signature...\n");
+        fread(bsig, sig_size, 1, f);
+        total_read += sig_size;
+        fwrite(bsig, sig_size, 1, r);
+        fclose(fsig);
+    }
+
     fclose(f);
-    
+
     //printf("Total Read: %d\n", total_read);
     return 0;
 }
