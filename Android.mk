7f1746f8e0cbd64d5a4e89278836b076f4a5b125
AKuHAK <akuhak@gmail.com>
2017-06-28 13:54:56 +0300

    Added more recognized models, changed tool name

--- Android.mk
+++ Android.mk
@@ -5,27 +5,27 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := mkbootimg.c
 LOCAL_STATIC_LIBRARIES := libmincrypt
 
-LOCAL_MODULE := pxa1088-mkbootimg
+LOCAL_MODULE := pxa1xx8-mkbootimg
 
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackbootimg.c
-LOCAL_MODULE := pxa1088-unpackbootimg
+LOCAL_MODULE := pxa1xx8-unpackbootimg
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dtbtool.c
 LOCAL_CFLAGS += -Wall
-LOCAL_MODULE := pxa1088-dtbTool
+LOCAL_MODULE := pxa1xx8-dtbTool
 include $(BUILD_HOST_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := mkbootimg.c
 LOCAL_STATIC_LIBRARIES := libmincrypt libcutils libc
-LOCAL_MODULE := utility_pxa1088-mkbootimg
+LOCAL_MODULE := utility_pxa1xx8-mkbootimg
 LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_STEM := pxa1088-mkbootimg
+LOCAL_MODULE_STEM := pxa1xx8-mkbootimg
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
@@ -35,9 +35,9 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := unpackbootimg.c
 LOCAL_STATIC_LIBRARIES := libcutils libc
-LOCAL_MODULE := utility_pxa1088-unpackbootimg
+LOCAL_MODULE := utility_pxa1xx8-unpackbootimg
 LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_STEM := pxa1088-unpackbootimg
+LOCAL_MODULE_STEM := pxa1xx8-unpackbootimg
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
@@ -48,9 +48,9 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := dtbtool.c
 LOCAL_CFLAGS += -Wall
 LOCAL_STATIC_LIBRARIES := libcutils libc
-LOCAL_MODULE := utility_pxa1088-dtbTool
+LOCAL_MODULE := utility_pxa1xx8-dtbTool
 LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_STEM := pxa1088-dtbTool
+LOCAL_MODULE_STEM := pxa1xx8-dtbTool
 LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
 LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
--- Makefile
+++ Makefile
@@ -17,7 +17,7 @@ else
     LDFLAGS += -Wl,--gc-sections
 endif
 
-all:libmincrypt.a pxa1088-mkbootimg$(EXE) pxa1088-unpackbootimg$(EXE) pxa1088-dtbTool$(EXE)
+all:libmincrypt.a pxa1xx8-mkbootimg$(EXE) pxa1xx8-unpackbootimg$(EXE) pxa1xx8-dtbTool$(EXE)
 
 static:
 	make LDFLAGS="$(LDFLAGS) -static"
@@ -25,26 +25,26 @@ static:
 libmincrypt.a:
 	make -C libmincrypt
 
-pxa1088-mkbootimg$(EXE):mkbootimg.o
+pxa1xx8-mkbootimg$(EXE):mkbootimg.o
 	$(CROSS_COMPILE)$(CC) -o $@ $^ -L. -lmincrypt $(LDFLAGS) -s
 
 mkbootimg.o:mkbootimg.c
 	$(CROSS_COMPILE)$(CC) -o $@ $(CFLAGS) -c $< -I. -Werror
 
-pxa1088-unpackbootimg$(EXE):unpackbootimg.o
+pxa1xx8-unpackbootimg$(EXE):unpackbootimg.o
 	$(CROSS_COMPILE)$(CC) -o $@ $^ $(LDFLAGS) -s
 
 unpackbootimg.o:unpackbootimg.c
 	$(CROSS_COMPILE)$(CC) -o $@ $(CFLAGS) -c $< -Werror
 
-pxa1088-dtbTool$(EXE):dtbtool.o
+pxa1xx8-dtbTool$(EXE):dtbtool.o
 	$(CROSS_COMPILE)$(CC) -o $@ $^ $(LDFLAGS) -s
 
 dtbtool.o:dtbtool.c
 	$(CROSS_COMPILE)$(CC) -o $@ $(CFLAGS) -c $< -Werror
 
 clean:
-	$(RM) mkbootimg.o pxa1088-mkbootimg pxa1088-mkbootimg.exe unpackbootimg.o pxa1088-unpackbootimg pxa1088-unpackbootimg.exe dtbtool.o pxa1088-dtbTool pxa1088-dtbTool.exe
-	$(RM) libmincrypt.a Makefile.~
+	$(RM) pxa1xx8-mkbootimg *.exe *.o pxa1xx8-unpackbootimg pxa1xx8-dtbTool
+	$(RM) libmincrypt.a *.~
 	make -C libmincrypt clean
 
