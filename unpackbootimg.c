b69dc863600123ae0680ec3038e5b224a589cba1
osm0sis <osm0sis@outlook.com>
2019-04-13 09:10:56 -0300

    unpackbootimg: rename dtb to dt to not conflict with AOSP adding --dtb

--- unpackbootimg.c
+++ unpackbootimg.c
@@ -241,7 +241,7 @@ int main(int argc, char** argv)
 
     if (header.dt_size != 0) {
         sprintf(tmp, "%s/%s", directory, basename(filename));
-        strcat(tmp, "-dtb");
+        strcat(tmp, "-dt");
         FILE *d = fopen(tmp, "wb");
         byte* dt = (byte*)malloc(header.dt_size);
         //printf("Reading dt...\n");
